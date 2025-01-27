47fcb44471a99d4902eedce21c06c2342e9a0dd7
Mitesh Jain <miteshjain2808@gmail.com>
2025-01-27 12:08:32 +0530

    Time: 63 ms (50.53%) | Memory: 20.5 MB (34.20%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        def dfs(i):
+            if dp[i]:
+                return adj_set[i]
+
+            if not adj_set[i]:
+                dp[i] = True
+                return set()
+
+            cur = adj_set[i].copy()
+            for j in cur:
+                adj_set[i] |= dfs(j)
+
+            dp[i] = True
+            return adj_set[i]
+
+        adj_set = [set() for _ in range(numCourses)]
+        dp = [False for _ in range(numCourses)]
+        for a,b in prerequisites:
+            adj_set[a].add(b)
+
+        for i in range(numCourses):
+            cur = adj_set[i].copy()
+            for j in cur:
+                adj_set[i] |= dfs(j)
+        
+        n = len(queries)
+        res = [False for _ in range(n)]
+        for i in range(n):
+            if queries[i][1] in adj_set[queries[i][0]]:
+                res[i] = True
+
+        return res
