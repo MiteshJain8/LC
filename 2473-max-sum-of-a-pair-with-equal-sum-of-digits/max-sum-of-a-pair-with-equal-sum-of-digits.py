29edc99938a67e1d97c8e9303a3b7998bd356f93
Mitesh Jain <miteshjain2808@gmail.com>
2025-02-12 09:12:49 +0530

    Time: 293 ms (81.05%) | Memory: 33.2 MB (89.08%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,6 +1,6 @@
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        prefix = defaultdict(list)
+        prefix = [0] * 82
         n, res = len(nums), -1
         for i in range(n):
             num = nums[i]
@@ -8,10 +8,9 @@ class Solution:
             while num:
                 dig += num % 10
                 num //= 10
-            heappush(prefix[dig], -nums[i])
-        for pq in prefix.values():
-            if len(pq) > 1:
-                n1 = -heappop(pq)
-                n2 = -heappop(pq)
-                res = max(res, n1+n2)
+            if prefix[dig]:
+                res = max(res, nums[i]+prefix[dig])
+                prefix[dig] = max(nums[i], prefix[dig])
+            else:
+                prefix[dig] = nums[i]
         return res
