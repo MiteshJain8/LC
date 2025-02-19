23b69f3e1fd5110d707608cb016e2f6a00e65d35
Mitesh Jain <miteshjain2808@gmail.com>
2025-02-19 07:17:26 +0530

    Time: 4 ms (76.37%) | Memory: 17.6 MB (99.12%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        tot = 3 * 2 ** (n-1)
+        res = []
+        choices = "abc"
+        left, right = 1, tot
+        for _ in range(n):
+            cur = left
+            size = (right - left + 1) // len(choices)
+            for c in choices:
+                if cur <= k < cur + size:
+                    res.append(c)
+                    left = cur
+                    right = cur + size - 1
+                    choices = "abc".replace(c, '')
+                    break
+                cur += size
+        return "".join(res)
