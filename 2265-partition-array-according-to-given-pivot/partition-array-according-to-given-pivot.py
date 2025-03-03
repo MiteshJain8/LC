b3f0375dbfde314566d910646bf8508389718ca7
Mitesh Jain <miteshjain2808@gmail.com>
2025-03-03 07:43:43 +0530

    Time: 115 ms (5.71%) | Memory: 33.6 MB (95.77%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,12 +1,9 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        n, j = len(nums), 0
+        j = 0
         res = sorted(nums)
-        idx = res.index(pivot)
-        while idx < n-1 and res[idx+1] == pivot:
-            idx += 1
-        idx += 1
-        for i in range(n):
+        idx = bisect.bisect_right(res, pivot)
+        for i in range(len(nums)):
             if nums[i] < pivot:
                 res[j] = nums[i]
                 j += 1
