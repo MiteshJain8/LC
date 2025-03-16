1b7c2f4cdbf591dc17d5fa7fa0d8e340e15bb70b
Mitesh Jain <miteshjain2808@gmail.com>
2025-03-16 10:09:18 +0530

    Time: 201 ms (91.57%) | Memory: 22 MB (49.42%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -3,14 +3,13 @@ class Solution:
         def is_possible(time):
             k = 0
             for rank in ranks:
-                k += floor(sqrt(time/rank))
+                k += floor(sqrt(time//rank))
                 if k >= cars:
                     return True
             return False 
 
         n = len(ranks)
-        ranks.sort(reverse = True)
-        left, right = ranks[-1] * ((cars//n) ** 2), ranks[0] * ((ceil(cars/n)) ** 2)
+        left, right = (cars//n) ** 2, max(ranks) * ((ceil(cars/n)) ** 2)
         res = 0
         while left <= right:
             mid = left + (right-left)//2
