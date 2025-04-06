105edf4eb575d34259ffc601d53903cd3a4c6d3e
Mitesh Jain <miteshjain2808@gmail.com>
2025-04-06 11:31:21 +0530

    Time: 51 ms (97.98%) | Memory: 60.8 MB (81.50%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,16 +1,11 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        def backtrack(i):
-            if i >= n:
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-
-            res1 = backtrack(i+1)
-            res2 = backtrack(i+1+questions[i][1]) + questions[i][0]
-            dp[i] = max(res1, res2)
-            return dp[i]
-
         n = len(questions)
-        dp = [-1] * n
-        return backtrack(0)
+        dp = [questions[i][0] for i in range(n)]
+        for i in range(n-2, -1, -1):
+            j = i+1+questions[i][1]
+            if j < n and dp[j] + questions[i][0] > dp[i+1]:
+                dp[i] = dp[j] + questions[i][0]
+            elif dp[i+1] > dp[i]:
+                dp[i] = dp[i+1]
+        return dp[0]
