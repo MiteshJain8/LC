e85057bb6a8bf22de8d9d589438c72528871347b
Mitesh Jain <miteshjain2808@gmail.com>
2025-06-24 10:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (13.13%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,19 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def dfs(i):
-            # end
+        def  dfs(i):
             if i >= n:
                 return 0
-            if i == n-1:
-                return nums[i]
             if dp[i] != -1:
                 return dp[i]
-            # rob
-            rob = nums[i] + dfs(i+2)
-            # no rob
-            no_rob = dfs(i+1)
 
-            dp[i] = max(rob, no_rob)
+            dp[i] = dfs(i + 2) + nums[i]
+
+            dp[i] = max(dfs(i + 1), dp[i])
             return dp[i]
 
         n = len(nums)
