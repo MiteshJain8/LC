fa23603ce346f0e8aee4d9806b4e6a7d6e0d758c
Mitesh Jain <miteshjain2808@gmail.com>
2025-07-13 09:51:16 +0530

    Time: 163 ms (5.33%) | Memory: 32.9 MB (95.92%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        heapify(players)
+        heapify(trainers)
+        res = 0
+        while players and trainers:
+            if players[0] <= trainers[0]:
+                res += 1
+                heappop(players)
+                heappop(trainers)
+            else:
+                heappop(trainers)
+
+        return res
