8593e20a2f262768e0949a0c071039134504e496
Mitesh Jain <miteshjain2808@gmail.com>
2025-07-28 09:28:39 +0530

    Time: 1412 ms (7.73%) | Memory: 314.8 MB (5.09%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -4,17 +4,23 @@ class Solution:
             if i == n:
                 return 1 if cur_or == max_or else 0
 
+            if dp[i][cur_or] != -1:
+                return dp[i][cur_or]
+
             #leave
             leave = backtrack(i+1, cur_or)
 
             #take
             take = backtrack(i+1, cur_or | nums[i])
 
-            return take + leave
+            dp[i][cur_or] = take + leave
+
+            return dp[i][cur_or]
 
         n = len(nums)
         max_or = 0
         for num in nums:
             max_or |= num
+        dp = [[-1 for _ in range(max_or+1)] for _ in range(n)]
 
         return backtrack(0, 0)
