da37ffc2118db8bd4cb7c28bb1845c5dedfc9bca
Mitesh Jain <miteshjain2808@gmail.com>
2025-07-28 12:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (21.82%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -9,16 +9,12 @@ class Solution:
         for i in range(n-1, -1, -1):
             for j in range(i+1, n+1):
                 if dp[j] and s[i:j] in hset:
-                    print(s[i:j])
                     dp[i] = True
-                    print(res[j], j)
                     for k in range(len(res[j])):
                         if res[j][k] == "":
                             val = s[i:j]
                         else:
                             val = s[i:j] + " " + res[j][k]
-                        print(val, i, j)
                         res[i].append(val)
                         
-            print(res[i])   
         return res[0]
