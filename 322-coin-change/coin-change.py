5307b0b5c9b30df67ff4bb1163c5f1a57686a015
Mitesh Jain <miteshjain2808@gmail.com>
2025-07-30 12:53:56 +0530

    Time: 751 ms (60.90%) | Memory: 18.1 MB (88.00%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,20 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def backtrack(remaining):
-            if dp[remaining] != float("inf"):
-                return dp[remaining]
-
-            res = amount + 1
-            for c in coins:
-                if remaining - c < 0:
-                    break
-                res = min(res, 1 + backtrack(remaining - c))
-
-            dp[remaining] = res
-            return res
-
-        dp = [float("inf")] * (amount + 1)
+        dp = [amount+1] * (amount+1)
         dp[0] = 0
+        coins = list(set(coins))
         coins.sort()
-        ans = backtrack(amount)
-        return -1 if ans == amount + 1 else ans
+        for i in range(1, amount+1):
+            for c in coins:
+                if i - c < 0:
+                    break
+                dp[i] = min(dp[i], 1 + dp[i-c])
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
