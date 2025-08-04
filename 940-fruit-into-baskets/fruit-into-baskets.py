6cd7fc713aaac631e58e45e99d442523263cc02c
Mitesh Jain <miteshjain2808@gmail.com>
2025-08-04 18:10:52 +0530

    Time: 178 ms (70.17%) | Memory: 23.7 MB (42.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -2,18 +2,16 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         n = len(fruits)
         hset = defaultdict(int)
-        l = res = cur = 0
+        l = res = 0
         for r in range(n):
             hset[fruits[r]] += 1
             if len(hset) <= 2:
-                cur += 1
-                res = max(res, cur)
+                res = max(res, r - l + 1)
             else:
                 while len(hset) > 2:
                     hset[fruits[l]] -= 1
                     if hset[fruits[l]] == 0:
                         del hset[fruits[l]]
                     l += 1
-                cur = r - l + 1
 
         return res
