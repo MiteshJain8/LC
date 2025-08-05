49e1245e9d748a2c14e2325faef5853206d65665
Mitesh Jain <miteshjain2808@gmail.com>
2025-08-05 23:57:34 +0530

    Time: 40 ms (30.81%) | Memory: 55.7 MB (26.27%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,51 @@
+class Trie {
+    HashMap<Character, Trie> children;
+    boolean isEnd;
+    public Trie() {
+        children = new HashMap<Character, Trie>();
+        isEnd = false;
+    }
+    
+    public void insert(String word) {
+        Trie cur = this;
+        for (Character c: word.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                cur.children.put(c, new Trie());
+            }
+            cur = cur.children.get(c);
+        }
+        cur.isEnd = true;
+    }
+    
+    public boolean search(String word) {
+        Trie cur = this;
+        for (Character c: word.toCharArray()) {
+            if (cur.children.containsKey(c)) {
+                cur = cur.children.get(c);
+            } else {
+                return false;
+            }
+        }
+        return cur.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        Trie cur = this;
+        for (Character c: prefix.toCharArray()) {
+            if (cur.children.containsKey(c)) {
+                cur = cur.children.get(c);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
