b4b781b61ed79f8cebdbef996a97b1d6afd8bc78
Mitesh Jain <miteshjain2808@gmail.com>
2025-08-05 07:15:46 +0530

    Time: 19 ms (69.23%) | Memory: 17.8 MB (83.86%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -1,11 +1,10 @@
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         n = len(fruits)
-        occupied = [False] * n
         for f in fruits:
             for i in range(n):
-                if baskets[i] >= f and not occupied[i]:
-                    occupied[i] = True
+                if baskets[i] >= f:
+                    baskets[i] = 0
                     break
 
-        return occupied.count(False)
+        return n - baskets.count(0)
