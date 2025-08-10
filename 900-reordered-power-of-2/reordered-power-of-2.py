88ace4c9a9e71beea74468122e5d9f68524f3e90
Mitesh Jain <miteshjain2808@gmail.com>
2025-08-10 08:39:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (79.77%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -1,11 +1,4 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        for perm in itertools.permutations(str(n)):
-            if perm[0] == "0":
-                continue
-
-            val = int("".join(perm))
-            if (val & (val - 1)) == 0:
-                return True
-
-        return False
+        ctr = sorted(str(n))
+        return any(sorted(str(1<<i)) == ctr for i in range(32))
