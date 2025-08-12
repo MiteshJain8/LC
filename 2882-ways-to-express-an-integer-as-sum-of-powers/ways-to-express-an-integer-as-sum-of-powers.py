87ed0dd8d8f8bc3f0c0570b6180dad6ca977e163
Mitesh Jain <miteshjain2808@gmail.com>
2025-08-12 21:13:16 +0530

    Time: 2262 ms (12.79%) | Memory: 20 MB (46.22%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,21 +1,12 @@
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
-        def backtrack(rem, i):
-            if i ** x == rem:
-                return 1
-            if i ** x > rem:
-                return 0
-
-            if dp[rem][i] != -1:
-                return dp[rem][i]
-
-            res = backtrack(rem, i+1) % mod
-            res += backtrack(rem - i ** x, i+1) % mod
-
-            dp[rem][i] = res
-
-            return res
-
         mod = 10 ** 9 + 7
-        dp = [[-1] * (n+1) for _ in range(n+1)]
-        return backtrack(n, 1) % mod
+        dp = [[0] * (n+1) for _ in range(n+1)]
+        dp[0][0] = 1
+        for i in range(1,n+1):
+            val = i ** x
+            for j in range(n+1):
+                dp[i][j] = dp[i-1][j]
+                if j >= val:
+                    dp[i][j] += dp[i-1][j-val]
+        return dp[n][n] % mod
