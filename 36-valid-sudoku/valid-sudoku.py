1ebebcf32e3fcbd166e93314e6a8141bc590bc92
Mitesh Jain <miteshjain2808@gmail.com>
2025-08-30 14:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (44.24%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,13 +4,14 @@ class Solution:
         boxes = [[set() for _ in range(3)] for _ in range(3)]
         for i in range(9):
             for j in range(9):
-                if board[i][j] == ".":
+                ele = board[i][j]
+                if ele == ".":
                     continue
-                if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in boxes[i//3][j//3]:
+                if ele in rows[i] or ele in cols[j] or ele in boxes[i//3][j//3]:
                     return False
                 else:
-                    rows[i].add(board[i][j])
-                    cols[j].add(board[i][j])
-                    boxes[i//3][j//3].add(board[i][j])
+                    rows[i].add(ele)
+                    cols[j].add(ele)
+                    boxes[i//3][j//3].add(ele)
 
         return True
