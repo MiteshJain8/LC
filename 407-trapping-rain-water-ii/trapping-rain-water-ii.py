57cd9f3de4254046a9b3851f246adaeae4837f80
Mitesh Jain <miteshjain2808@gmail.com>
2025-10-03 12:29:17 +0530

    Time: 117 ms (87.64%) | Memory: 20.2 MB (72.63%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.py
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -1,31 +1,25 @@
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
-        if not heightMap or not heightMap[0]:
-            return 0
-        
         m, n = len(heightMap), len(heightMap[0])
-        visited = [[False] * n for _ in range(m)]
         heap = []
-        
+        visit = [[False] * n for _ in range(m)]
         for i in range(m):
             for j in [0, n-1]:
-                heapq.heappush(heap, (heightMap[i][j], i, j))
-                visited[i][j] = True
-        for j in range(1, n-1):
+                heappush(heap, (heightMap[i][j], i, j))
+                visit[i][j] = True
+        for j in range(1, n):
             for i in [0, m-1]:
-                heapq.heappush(heap, (heightMap[i][j], i, j))
-                visited[i][j] = True
-        
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        water = 0
-        
+                heappush(heap, (heightMap[i][j], i, j))
+                visit[i][j] = True
+        res = 0
+        dirs = [(0, -1), (0, 1), (-1, 0), (1, 0)]
         while heap:
-            height, x, y = heapq.heappop(heap)
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
-                    water += max(0, height - heightMap[nx][ny])
-                    visited[nx][ny] = True
-                    heapq.heappush(heap, (max(heightMap[nx][ny], height), nx, ny))
-        
-        return water
+            val, i, j = heappop(heap)
+            for dx, dy in dirs:
+                x, y = i + dx, j + dy
+                if 0 <= x < m and 0 <= y < n and not visit[x][y]:
+                    visit[x][y] = True
+                    res += max(0, val - heightMap[x][y])
+                    heappush(heap, (max(heightMap[x][y], val), x, y))
+
+        return res
