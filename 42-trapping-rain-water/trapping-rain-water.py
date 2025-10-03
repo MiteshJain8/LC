5eedae7306f085e83c531eeef810cc01ac0cfdb2
Mitesh Jain <miteshjain2808@gmail.com>
2025-10-03 12:48:06 +0530

    Time: 15 ms (51.85%) | Memory: 19.4 MB (38.89%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,17 @@
 class Solution:
     def trap(self, list1: List[int]) -> int:
         n = len(list1)
-        heap = []
-        visit = [False] * n
-        heappush(heap, (list1[0], 0))
-        heappush(heap, (list1[n-1], n-1))
-        visit[0], visit[n-1] = True, True
         res = 0
-        while heap:
-            val, i = heappop(heap)
-            if 0 <= i-1 and not visit[i-1]:
-                visit[i-1] = True
-                res += max(0, val - list1[i-1])
-                heappush(heap, (max(val, list1[i-1]), i-1))
-            if n > i+1 and not visit[i+1]:
-                visit[i+1] = True
-                res += max(0, val - list1[i+1])
-                heappush(heap, (max(val, list1[i+1]), i+1))
+        left, right = 0, n - 1
+        left_max, right_max = list1[0], list1[n-1]
+        while left < right:
+            if list1[left] < list1[right]:
+                left += 1
+                res += max(0, left_max - list1[left])
+                left_max = max(left_max, list1[left])
+            else:
+                right -= 1
+                res += max(0, right_max - list1[right])
+                right_max = max(right_max, list1[right])
 
         return res
