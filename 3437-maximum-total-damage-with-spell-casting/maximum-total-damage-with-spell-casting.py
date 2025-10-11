67de0e1f37c6ff36cb916e4ae84197fa80a241af
Mitesh Jain <miteshjain2808@gmail.com>
2025-10-11 22:02:23 +0530

    Time: 632 ms (43.25%) | Memory: 211.7 MB (16.00%) - LeetSync

---
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        @lru_cache(None)
+        def fun(i):
+            if i>=n:
+                return 0
+            take_dam = count[unique[i]] * unique[i]
+            next_i = i+1
+            while next_i < n and unique[next_i] <= unique[i]+2:
+                next_i += 1
+            take = take_dam  + fun(next_i)
+            not_take = fun(i+1)
+            return max(take,not_take)
+
+        count = Counter(power)
+        unique = sorted(count.keys())
+        n = len(unique)
+        return fun(0)
