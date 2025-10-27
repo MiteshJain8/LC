aa7ea4ed795471c36e2a3b431ec2ab2bcec3a15d
Mitesh Jain <miteshjain2808@gmail.com>
2025-10-27 11:33:29 +0530

    Time: 3 ms (93.37%) | Memory: 19.8 MB (7.41%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        m = len(bank)
+        cnt = [s.count('1') for s in bank]
+        print(cnt)
+        res = 0
+        prev = cnt[0]
+        for i in range(1, m):
+            if cnt[i]:
+                res += (prev * cnt[i])
+                prev = cnt[i]
+
+        return res
